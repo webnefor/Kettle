fdbbff66e5a5d577eb82655e6359baee2a889ca8
Lucas <113185077+seout@users.noreply.github.com>
2024-02-25 21:19:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,34 @@
 int main(int argc, char *argv[]) {
 
     struct opt::options_flag conf = opt::parse(argc, argv);
-
     CoreServer Kernel(conf);
 
     while (true) {
-        std::vector <std::thread> tsplit;
-        try {
-            for (int i = 0; i < conf.thread; i++) {
-                tsplit.push_back(std::thread(&CoreServer::start, &Kernel));
-            }
 
-            for (auto &th: tsplit) {
+        std::vector<std::thread> tsplit;
+
+        try {
+
+            for (int i = 0; i < conf.thread; i++)
+
+                if (conf.mode == "http")
+                    tsplit.push_back(std::thread(&CoreServer::start_http, &Kernel));
+
+                else if (conf.mode == "icmp")
+                    tsplit.push_back(std::thread(&CoreServer::start_icmp, &Kernel));
+
+                else {
+                    opt::show_help();
+                    exit(-1);
+                }
+
+            for (auto &th: tsplit)
                 th.join();
-            }
 
             tsplit.clear();
-        } catch (const std::exception &err) {
+        }
+        catch (const std::exception &err)
+        {
             std::cerr << err.what() << std::endl;
             continue;
         }
