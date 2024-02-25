a9763283b33093d008bab236561d5239c42854f6
Lucas <113185077+seout@users.noreply.github.com>
2024-02-25 21:23:02 +0300

    Update icmp.cpp

---
+++ src/icmp.cpp
@@ -0,0 +1,16 @@
+#include "config.hpp"
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <cstring>
+
+
+int CoreServer::start_icmp() {
+
+
+
+    return 0;
+}
