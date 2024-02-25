686bc57cd88588ab152059f25d0f6be3e24e57d2
Lucas <113185077+seout@users.noreply.github.com>
2024-02-25 13:05:38 +0300

    Update http.cpp

--- src/http.cpp
+++ src/http.cpp
@@ -1,24 +1,23 @@
 
 #include "config.hpp"
 
-
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/select.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <cstring>
-#include <stdlib.h>
-#include <fcntl.h>
-
 
 int CoreServer::start() {
 
-    char buff[1024];
-    unsigned int sock, result, status, flags;
     socklen_t strsize;
-    struct sockaddr_in configuration;
+    fd_set wset;
+
+    struct sockaddr_in configuration{};
+    struct timeval tv{};
+
+    char buff[1024];
+    unsigned int sock, result, status, flags,yes =1;
 
     configuration.sin_addr.s_addr = inet_addr(
             CoreServer::host.c_str()
@@ -27,60 +26,49 @@ int CoreServer::start() {
             CoreServer::port
             );
     configuration.sin_family      = AF_INET;
+//    flags = fcntl(sock, F_GETFL, 0);
 
     strsize = sizeof configuration;
 
-    struct timeval tv;
-
-    tv.tv_sec = 2;
+    tv.tv_sec = 1;
     tv.tv_usec = 0;
-
-    fd_set wset;
 
     try {
 
         sock = socket(AF_INET, SOCK_STREAM, 0);
 
-        if (fcntl(sock, F_SETFL, O_NONBLOCK | O_ASYNC) < 0) {
-            perror("");
+        if (sock == -1) {
+            perror("socket[-]");
+            close(sock);
+            return -1;
         }
+
+        result = connect(sock, (struct sockaddr *)&configuration, strsize);
 
         FD_ZERO(&wset);
-
         FD_SET(sock, &wset);
 
-        status = connect(sock, (struct sockaddr *) &configuration, strsize);
+        if (result != 0)
+            status = select(sock, NULL, &wset, NULL, &tv);
 
-        if (errno != EINPROGRESS && errno != EWOULDBLOCK) {
-            perror("[\033[033m]\n");
-//            close(sock);
+        send(sock, HEADER, strlen(HEADER), 0);
+        recv(sock, buff, 1024, 0);
+
+        if (status == -1) {
+            close(sock);
+            perror("if");
+            return -1;
         }
 
-        if (status < 0)
-            result = select(sock + 1, NULL, &wset, NULL, &tv);
-            send(sock, HEADER, strlen(HEADER), 0);
-
-        if (result == 0)
-        {
-            printf("\033[031m(%s) <501> [-]\n",CoreServer::host.c_str());
-//            close(sock);
-        }
-
-        else if (result < 0) {
-            perror("[\033[033m]\n");
-//            close(sock);
-//            exit(1);
-        }
-
-        printf("\033[032m(%s) <200> [+]\n",CoreServer::host.c_str());
+        printf("\033[032m64 bytes to (%s) [+]\n", CoreServer::host.c_str());
 
         close(sock);
 
-
-    } catch (const std::exception &er) {
-        std::cerr << er.what() << std::endl;
     }
-
-
+    catch (const std::exception &er)
+    {
+        std::cerr << er.what() << std::endl;
+        return -1;
+    }
     return 0;
 }
