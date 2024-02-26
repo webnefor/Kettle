f9c9aaf4dd1d74c18b2da1c41c24ae8d3d657189
Lucas <113185077+seout@users.noreply.github.com>
2024-02-26 19:17:21 +0300

    Update http.cpp

--- src/http.cpp
+++ src/http.cpp
@@ -13,8 +13,12 @@ int CoreServer::start_http() {
     socklen_t strsize;
     fd_set wset;
 
-    struct sockaddr_in configuration{};
-    struct timeval tv{};
+    struct sockaddr_in configuration{
+
+    };
+    struct timeval tv{
+        1,0
+    };
 
     unsigned int sock, result, status, flags,yes =1;
 
@@ -28,42 +32,38 @@ int CoreServer::start_http() {
 
     strsize = sizeof configuration;
 
-    tv.tv_sec = 1;
-    tv.tv_usec = 0;
-
     try {
 
         sock = socket(AF_INET, SOCK_STREAM, 0);
-
-        if (sock == -1) {
-            close(sock);
-            return -1;
-        }
 
         result = connect(sock, (struct sockaddr *)&configuration, strsize);
 
         FD_ZERO(&wset);
         FD_SET(sock, &wset);
 
-        if (result != 0)
-            status = select(sock, NULL, &wset, NULL, &tv);
+        if (result <= 0)
+            status = select(sock + 1, NULL, &wset, NULL, &tv);
 
-        send(sock, HEADER, strlen(HEADER), 0);
-
-        if (status == -1) {
+        if (status <= 0 || status == -1)
+        {
             close(sock);
             return -1;
         }
 
-        printf("\033[032m64 bytes to (%s):%i\n", CoreServer::host.c_str(),CoreServer::port);
-
-        close(sock);
+        send(sock, HEADER, strlen(HEADER), 0);
 
     }
     catch (const std::exception &er)
     {
         std::cerr << er.what() << std::endl;
+        close(sock);
+
         return -1;
     }
+
+    printf("\033[032m64 bytes to (%s):%i\n", CoreServer::host.c_str(),CoreServer::port);
+
+    close(sock);
+
     return 0;
 }
