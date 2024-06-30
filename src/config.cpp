6ffa923ba3dcf9affd2a3f7e3708959a55443de8
NEFOR <113185077+webnefor@users.noreply.github.com>
2024-06-30 20:40:49 +0300

    Update config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -57,7 +57,7 @@ struct opt::options_flag opt::parse(int argc, char *argv[]) {
     }
 
     if (set_options.mode)
-        if (geteuid() !== 0) {
+        if (geteuid() != 0) {
 //          printf("\033[031m[%i]",args->port);
             printf("\033[031mNot running with root permissions.\n");
             exit(-1);
